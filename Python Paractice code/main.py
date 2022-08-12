16caea06b2a4678e8315d1413b0bb91ad897a524
Jawad Ali <jawadali000123@gmail.com>
2022-08-12 23:19:14 +0500

    Commit 1

---
+++ Python Paractice code/main.py
@@ -0,0 +1,57 @@
+print('')
+print('---------- Print a string/character etc-------')
+print(5)                #printing a number
+print('hello World')    #printing a string
+print('A')              #printing a Character
+print(4.5)              #printing a Floating point number
+print('@$$Y^&#%(#')     #printing random characters
+print('*' * 10)         #printing a character or pattern for multiple times
+print('\'\'')           #printing '' quotation marks
+print("''")             #printing '' quotations marks
+
+
+print('----------- Variables -------------')
+price1 = 10      #price is 10
+price1 = 20      #price is now updated to 20
+rating1 = 4.7    #Floating point number
+name1 = 'Alex A'   #Printing a string or a character
+published1 = False #For true or false/ boolean conditions
+print(price1)
+print(rating1)
+print(name1)
+print(published1)
+
+print('----------- Variables Exercise -------------')
+name1 = 'John Smith'
+Age1 = 20
+new_patient1 = True
+existing_patient1 = True
+
+
+print('----------- Input -------------')
+name1 = input('Enter name ')        #input name
+print('Hello ' + name1)             #print name
+Age1 = input('Enter your Age ')     #input Age
+print('Age =' + Age1)              #print Age
+print('New Patient')
+print(new_patient1)
+
+print('----------- Input Exercise -------------')
+name2 = input('Person\'s Name = ')                  #input name
+color2 = input('Person\'s Favourite color = ')      #input color
+print(name2 + ' Likes ' + color2)                   #print name Likes color
+
+print('----------- Type Conversion -------------')
+birth_year3 = input('Birth Year: ')     #input Birth Year
+print(type(birth_year3))                #print type of birth_year3
+Age3 = 2022 - int(birth_year3)          #convert birth_year3 String into int to calculate age
+print(type(Age3))                       #print type of Age3
+print(Age3)
+
+print('----------- Type Conversion Exercise -------------')
+
+weight_lbs3 = input('Enter your weight(in pounds/lbs) ')        #input weight
+weight_kg3 = float(weight_lbs3)/2.20462                        #convert pounds into kg( kg = pound/2.20462
+print(weight_kg3)
+
+print('----------- Strings -------------')
